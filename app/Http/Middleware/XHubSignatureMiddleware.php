24e76ece8aa46ee4a14b03a1d2c60363eee3b3a5
Simon Dann <simon.dann@gmail.com>
2019-08-23 23:21:56 +0100

    bugfix: correct X-Hub check

--- app/Http/Middleware/XHubSignatureMiddleware.php
+++ app/Http/Middleware/XHubSignatureMiddleware.php
@@ -20,8 +20,8 @@ class XHubSignatureMiddleware
             return response('Missing X-Hub-Signature header. Did you configure secret token in hook settings?', 422);
         }
 
-        if ($request->header('X-Hub-Signature') !== env('X_HUB_SIGNATURE')) {
-            return response('Incorrect X-Hub-Signature.', 401);
+        if ($request->header('X-Hub-Signature') !== 'sha1=' . hash_hmac('sha1', $request->getContent(), env('X_HUB_SIGNATURE'), false )) {
+            return response(' - Incorrect X-Hub-Signature.', 401);
         }
 
         return $next($request);
